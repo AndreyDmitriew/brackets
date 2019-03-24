e1cc6a6d4952ee8cd5afe77866b66c4a270fac4a
Andrey <Jarerrag@gmail.com>
2019-03-24 22:15:22 +0300

    Done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  module.exports = function check(str, bracketsConfig) {
+    let drive = [];
+
+    for (let i= 0; i < str.length; i++) {
+      for (let x = 0; x < bracketsConfig.length; x++) {
+        if(bracketsConfig[x][0] === str[i])  {
+          if(str[i] === drive[drive.length - 1] && bracketsConfig[x][0] === bracketsConfig[x][1]) {
+            drive.pop();
+            break;
+          }
+          drive.push(str[i]);
+          break;
+        }
+        if(bracketsConfig[x][1] === str[i]){
+          if(bracketsConfig[x][0] === drive[drive.length - 1]) {
+            drive.pop()
+            break;
+          } else return false;
+        }
+      }
+    }
+    return drive.length === 0;
+  }
+
